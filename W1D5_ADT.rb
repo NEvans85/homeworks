e5d31513bb8ff60a0817397632378824fc1269db
Nicholas Evans <nevans85.pro@gmail.com>
2017-08-03 19:37:46 -0700

    W1D5 write functions - still need to test

---
+++ W1D5_ADT.rb
@@ -0,0 +1,72 @@
+class Stack
+  def initialize
+    @elements = []
+  end
+
+  def add(el)
+    @elements.push(el)
+  end
+
+  def remove
+    @elements.pop
+  end
+
+  def show
+    puts @elements
+  end
+end
+
+class Queue
+  def initialize
+    @elements = []
+  end
+
+  def enqueue(el)
+    @elements.push(el)
+  end
+
+  def dequeue
+    @elements.shift
+  end
+
+  def show
+    puts @elements
+  end
+end
+
+class Map
+  def initialize
+    @kv_pairs = []
+  end
+
+  def assign(key, value)
+    if keys.include?(key)
+      @kv_pairs[index_of(key)].last = value
+    else
+      @kv_pairs.push([key, value])
+    end
+  end
+
+  def lookup(key)
+    keys.include?(key) ? @kv_pairs[index_of(key)].last : nil
+  end
+
+  def remove(key)
+    @kv_pairs.delete_at(index_of(key)) if keys.include?(key)
+  end
+
+  def show
+    puts @kv_pairs
+  end
+
+  private
+
+  def keys
+    @kv_pairs.map { |kv_pair| kv_pair[0] }
+  end
+
+  def index_of(key)
+    keys.includes?(key) ? keys.index(key) : nil
+  end
+
+end
